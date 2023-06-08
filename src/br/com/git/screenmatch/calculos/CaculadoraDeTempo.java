f2f9a627c87d2a792f9b11630501254527325405
Satturker <Satturkerpatuim@gmail.com>
2023-06-08 19:13:38 -0300

    boa

--- src/br/com/git/screenmatch/calculos/CaculadoraDeTempo.java
+++ src/br/com/git/screenmatch/calculos/CaculadoraDeTempo.java
@@ -2,16 +2,21 @@ package br.com.git.screenmatch.calculos;
 
 import br.com.git.screenmatch.modelo.Filme;
 import br.com.git.screenmatch.modelo.Serie;
+import br.com.git.screenmatch.modelo.Titulo;
 
 public class CaculadoraDeTempo {
     private int tempoTotal = 0;
     public int getTempoTotal() {
         return this.tempoTotal;
     }
-    public void inclui(Filme f){
+    /*public void inclui(Filme f){
         this.tempoTotal += f.getDuracaoEmMinutos();
     }
     public void inclui(Serie s){
         this.tempoTotal += s.getDuracaoEmMinutos();
+    }*/
+    public void inclui(Titulo titulo){
+        this.tempoTotal += titulo.getDuracaoEmMinutos();
     }
+
 }
